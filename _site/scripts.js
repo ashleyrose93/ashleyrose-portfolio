f3372bcbad7759997173e18988d234ec7cd45cc5
Ashley <ashley.boyd@outlook.co.nz>
2019-07-12 23:43:02 +1000

    Hamburger menu added and functioning!

--- _site/scripts.js
+++ _site/scripts.js
@@ -1,3 +1,10 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 });
+
+$( document ).ready(function() {
+   $(".burger-button").click(function(){
+     $(".burger-button").toggleClass("active");
+     $(".burger-menu").toggleClass("active");
+   });
+});
